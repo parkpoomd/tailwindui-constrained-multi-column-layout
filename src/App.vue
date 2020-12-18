93c8ee49375d0f31ab648c7a726a389dd27aa93d
parkpoomd <parkpoom.d@gmail.com>
2020-12-18 11:16:40 +0700

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,212 @@
 <template>
-  <div class="h-screen bg-gray-200">App</div>
+  <div class="min-h-screen flex flex-col">
+    <!-- Navbar -->
+    <nav class="flex-shrink-0 bg-indigo-700">
+      <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <!-- Logo section -->
+          <div
+            class="flex items-center flex-shrink-0 px-2 lg:px-0 xl:w-64 space-x-1"
+          >
+            <svg
+              class="h-12 w-12 text-indigo-400"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z"
+              />
+            </svg>
+            <span class="text-white text-2xl font-bold tracking-wide"
+              >workflow</span
+            >
+          </div>
+
+          <!-- Search section -->
+          <div class="flex-1 flex justify-center lg:justify-end">
+            <div class="w-full px-2 lg:px-6">
+              <label for="search" class="sr-only">Search projects</label>
+              <div class="relative text-indigo-300 focus-within:text-gray-400">
+                <div
+                  class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"
+                >
+                  <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <input
+                  id="search"
+                  class="block w-full pl-10 pr-3 py-2 border border-transparent rounded-md leading-5 bg-indigo-400 bg-opacity-25 text-indigo-300 placeholder-indigo-300 focus:outline-none focus:bg-white focus:placeholder-gray-400 focus:text-gray-900 sm:text-sm transition duration-150 ease-in-out"
+                  placeholder="Search projects"
+                  type="search"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="flex lg:hidden">
+            <!-- Mobile menu button -->
+            <button
+              @click="isMobileMenuOpen = !isMobileMenuOpen"
+              class="inline-flex items-center justify-center p-2 rounded-md text-indigo-400 hover:text-white hover:bg-indigo-600 focus:outline-none focus:text-white transition duration-150 ease-in-out"
+            >
+              <svg
+                class="block h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <!-- Links section -->
+          <div class="hidden lg:block lg:w-80">
+            <div class="flex items-center justify-end">
+              <div class="flex">
+                <a
+                  href="#"
+                  class="px-3 py-2 rounded-md text-sm leading-5 font-medium text-indigo-200 hover:text-white focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out"
+                  >Documentation</a
+                >
+                <a
+                  href="#"
+                  class="px-3 py-2 rounded-md text-sm leading-5 font-medium text-indigo-200 hover:text-white focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out"
+                  >Support</a
+                >
+              </div>
+              <!-- Profile dropdown -->
+              <div class="ml-4 relative flex-shrink-0">
+                <div>
+                  <button
+                    @click="isUserMenuOpen = !isUserMenuOpen"
+                    class="flex text-sm rounded-full text-white focus:outline-none focus:ring-2 focus:ring-white transition duration-150 ease-in-out"
+                    id="user-menu"
+                    aria-label="User menu"
+                    aria-haspopup="true"
+                  >
+                    <img
+                      class="h-8 w-8 rounded-full"
+                      src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </button>
+                </div>
+                <!-- Profile dropdown panel -->
+                <transition
+                  enter-active-class="transition ease-out duration-100"
+                  enter-from-class="transform opacity-0 scale-95"
+                  enter-to-class="transform opacity-100 scale-100"
+                  leave-active-class="transition ease-in duration-75"
+                  leave-from-class="transform opacity-100 scale-100"
+                  leave-to-class="transform opacity-0 scale-95"
+                >
+                  <div
+                    v-show="isUserMenuOpen"
+                    class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg"
+                  >
+                    <div class="py-1 rounded-md bg-white shadow-sm">
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                        role="menuitem"
+                        >View Profile</a
+                      >
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                        role="menuitem"
+                        >Settings</a
+                      >
+                      <a
+                        href="#"
+                        class="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+                        role="menuitem"
+                        >Sign out</a
+                      >
+                    </div>
+                  </div>
+                </transition>
+                <!-- /Profile dropdown panel -->
+              </div>
+            </div>
+          </div>
+          <!-- /Links section -->
+        </div>
+      </div>
+
+      <!-- Mobile menu -->
+      <div v-if="isMobileMenuOpen" class="lg:hidden">
+        <div class="px-2 pt-2 pb-3 space-y-1">
+          <a
+            href="#"
+            class="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600 focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out"
+            >Documentation</a
+          >
+          <a
+            href="#"
+            class="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600 focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out"
+            >Support</a
+          >
+        </div>
+        <div class="pt-4 pb-3 border-t border-indigo-800">
+          <div class="px-2 space-y-1">
+            <a
+              href="#"
+              class="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600 focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out"
+            >
+              Your Profile
+            </a>
+            <a
+              href="#"
+              class="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600 focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out"
+            >
+              Settings
+            </a>
+            <a
+              href="#"
+              class="block px-3 py-2 rounded-md text-base font-medium text-indigo-200 hover:text-indigo-100 hover:bg-indigo-600 focus:outline-none focus:text-white focus:bg-indigo-600 transition duration-150 ease-in-out"
+            >
+              Sign out
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- /Mobile menu -->
+    </nav>
+    <!-- /Navbar -->
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      isUserMenuOpen: false,
+      isMobileMenuOpen: false,
+    };
+  },
 };
 </script>
